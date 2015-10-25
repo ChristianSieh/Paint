06fd4676469d343b095328804c12fa258fafcfa6
Sieh <1960681@linux05.mcs.sdsmt.edu>
2015-10-25 14:25:54 -0600

    Added DrawPanel class

--- src/paint/Paint.java
+++ src/paint/Paint.java
@@ -51,7 +51,7 @@ class MenuFrame extends JFrame implements KeyListener
 
         // add drawing panel to content pane
         Container container = getContentPane();
-        //container.add( new DrawPanel() );
+        container.add( new DrawPanel() );
 
         // menu bar
         JMenuBar menuBar = new JMenuBar();
@@ -208,5 +208,60 @@ class MenuFrame extends JFrame implements KeyListener
     {
         if ( event.getKeyCode() == 27 )
             System.exit( 0 );
+    }
+}
+
+/** DrawPanel class
+ *  This class configures the drawing panel to support mouse events.
+ */
+class DrawPanel extends JPanel implements MouseListener
+{
+    private int x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+    private boolean leftButtonPress = false;
+
+    // constructor: set up window
+    public DrawPanel()
+    {
+        // detect mouse click events
+        addMouseListener( this );
+    }
+
+    // must override the following MouseListener methods
+    public void mouseClicked( MouseEvent event ) { }
+    public void mouseEntered( MouseEvent event ) { }
+    public void mouseExited( MouseEvent event ) { }
+
+    // mouse button press events (start drawing)
+    public void mousePressed( MouseEvent event )
+    {
+        // check for left mouse button press
+        if ( SwingUtilities.isLeftMouseButton( event ) )
+        {
+            x1 = event.getX();
+            y1 = event.getY();
+            System.out.println( "Mouse left button click: (" + x1 + "," + y1 + ")" );
+            leftButtonPress = true;
+        }
+    }
+
+    // mouse button release events (finish drawing)
+    public void mouseReleased( MouseEvent event )
+    {
+        if ( leftButtonPress )
+        {
+            x2 = event.getX();
+            y2 = event.getY();
+            System.out.println( "Mouse left button release: (" + x2 + "," + y2 + ")" );
+            leftButtonPress = false;
+            repaint();
+        }
+    }
+
+    // paintComponent() is the display callback function
+    public void paintComponent( Graphics g )
+    {
+        super.paintComponent( g );			// call superclass's paint method
+        g.setColor( Color.BLUE );
+        g.drawLine( x1, y1, x2, y2 );
     }
 }
